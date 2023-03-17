37d11f8fa0b91043c285cf19da9dd81ddebb88e9
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-17 16:24:55 -0700

    C if and while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int in;
+
+	for (in = 0; in <= 10; in++)
+	printf("%d\n", in);
+	return (0);
+}
