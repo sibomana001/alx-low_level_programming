883faab8ddb3e703068825139905b53c47bba9e0
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-17 15:15:02 -0700

    C if and while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	while (alphabet = 'a', alphabet <= 'z', alphabet++)
+	putchar("alphabet");
+	putchar("\n");
+	return (0);
+}
