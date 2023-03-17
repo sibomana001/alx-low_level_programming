70e3b4efe0e56e640dddb9d81e701816c582e204
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-17 16:57:31 -0700

    c if and while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	putchar(num);
-	for (letter = 'A'; letter <= 'F'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 	putchar(letter);
 	putchar('\n');
 	return (0);
