11d76908104bb75dd6003b82c9a21dbd46910208
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-17 16:41:39 -0700

    c if and while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int in;
+
+	for (in = '0'; in <= '9'; in++)
+	putchar(in);
+	putchar('\n');
+	return (0);
+}
