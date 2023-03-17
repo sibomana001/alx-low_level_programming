4845822dc38d245c2402d25c192111a555f25afe
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-17 15:29:42 -0700

    C if and while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	char alphabet;
 
-	while (alphabet = 'a', alphabet <= 'z', alphabet++)
-	putchar("alphabet");
-	putchar("\n");
+	for (alphabet = 'a', alphabet <= 'z', alphabet++)
+	putchar(alphabet);
+	putchar('\n');
 	return (0);
 }
