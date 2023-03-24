2aa3e0cb61ccf1789e643bf7e3f2eada94885728
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-24 05:46:18 -0700

    User defined functions

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit -  function that prints the last digit of a number
+ * @num: variable name
+ * Return: value of last digit
+ */
+int print_last_digit(int num)
+{
+	int l_digit
+	l_digit = num % 10;
+	return (l_digit);
+}
