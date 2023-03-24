e149b47c0e211a8218125860cf07980b00b3fb6f
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-24 05:20:17 -0700

    User defined functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs -  function that computes the absolute value of an integer
+ * @num: variable name
+ * Return:  Absolute value
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+		return (n);
+	if (num < 0)
+		return (-n);
+}
