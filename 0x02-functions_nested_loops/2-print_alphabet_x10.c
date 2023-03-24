832b1abe81a5866581f6a0c29002f53c32a2988b
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-24 00:53:31 -0700

    Alphabet printing 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: Printing alphabets ten times
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+	for (i = 97; i <= 122; i++)
+	{
+	_putchar(i);
+	}
+	continue;
+	}
+	_putchar('\n');
+}
