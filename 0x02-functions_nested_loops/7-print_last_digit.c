7ce08867462d74cd4ad534074dacfc3f52752f43
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-24 08:29:54 -0700

    User defined function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,10 @@
 int print_last_digit(int num)
 {
 	int last;
-
-	if (num < 0)
-		num = -num;
-	else
-		num = num;
+	
 	last = num % 10;
+	if (last < 0)
+		last = -last;
 _putchar(last + '0');
 return (last);
 }
