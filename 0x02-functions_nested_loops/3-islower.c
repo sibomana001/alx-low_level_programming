aabf09c1f814bae827e7cbfba74a263a5cb879c2
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-24 04:34:09 -0700

    User defined functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
  * _islower - Entry point
+ * @c: variable name
  * Description: function that checks for lowercase character
  * Return: Always 1 if 'c' is lowercase,otherwise returns 0
  */
 int _islower(int c)
 {
+	int c;
+
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
