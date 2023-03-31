17a2bc17d14ac814b242d0c972c4cc9035264fc2
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-31 05:08:08 -0700

    Pointers-arrays-strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * *_strcat - function that concatenates two strings.
+ * @dest: string to be concatenated on
+ * @src: String to concatenate on dest string
+ * Return:Concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length1, a,  length2, b;
+
+	length1 = 0;
+	length2 = 0;
+	{
+	while (dest[length1] != '\0')
+	lenght1++;
+	}
+	{
+	while (src[length2] != '\0' || src[length2] == '\0')
+	length2++;
+	}
+	a = length1;
+	b = lenght2;
+	for (a = 0, a >= 0, a++)
+	{
+	_putchar(dest[a]);
+	}
+	for (b = 0, b >= 0, b++)
+	{
+	_putchar(src[b]);
+	}
+	_putchar('\n');
+}
