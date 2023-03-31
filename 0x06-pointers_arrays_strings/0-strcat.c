dc6cd44fd3f8b01120cee29f181826164a4d76a2
sibomana001 <sibomana.adolphe007@gmail.com>
2023-03-31 08:56:01 -0700

    Pointers-arrays-strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,20 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * *_strcat - function that concatenates two strings.
  * @dest: string to be concatenated on
  * @src: String to concatenate on dest string
  * Return:Concatenated string
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 char *_strcat(char *dest, char *src)
 {
 	int length1, a,  length2, b;
