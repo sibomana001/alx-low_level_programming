4b1bb7387f4d170711217532310c8b4ccaae2ce9
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-01 14:08:33 -0700

    C functions& nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,9 +6,9 @@
  */
 int _isdigit(int c)
 {
-	if (c == 0 || c == 1 || c == 2 || c == 3 || c == 4 || c == 5)
+	if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5')
 		return (1);
-	else if (c == 6 || c == 7 || c == 8 || c == 9)
+	else if (c == '6' || c == '7' || c == '8' || c == '9')
 		return (1);
 	else
 		return (0);
