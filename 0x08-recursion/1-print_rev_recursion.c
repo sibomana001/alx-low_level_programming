d2b324f6a94d1c4af17f118312152ab3b00dc3a7
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-05 10:28:26 -0700

    C recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,8 +7,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		s--;
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
+		return;
 	}
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 }
