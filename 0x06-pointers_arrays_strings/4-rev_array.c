fac8e67c373eef607289fa5feedca8aa96f0d9aa
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-05 02:27:58 -0700

    pointers-arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: Array name
+ * @n: number of array elements to reverse
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = n; i > 0; i--)
+	{
+		j = 0;
+		do {
+		a[j] =  a[i];
+		} while (j < n);
+			j++;
+	}
+}
