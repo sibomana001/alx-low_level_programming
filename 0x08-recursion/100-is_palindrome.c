39f4a295c27bec79ff23d9116b20272c9650c562
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-06 00:49:30 -0700

    C recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -21,12 +21,12 @@ int _strlen_recursion(char *s)
 int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
-			{
-			if (n1 == n2 || n1 == n2 + 1)
-				return (1);
-			else
-				return (0 + comparator(s, n1 + 1, n2 - 1));
-			}
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		else
+			return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
 	return (0);
 }
 /**
@@ -40,5 +40,5 @@ int is_palindrome(char *s)
 	if (*s == '\0')
 		return (1);
 	else
-		return (comparator(s, 0, _strlen_recursion(s - 1)));
+		return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
