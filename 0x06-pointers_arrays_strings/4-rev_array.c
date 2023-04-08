f0c2db1ec200fb8eee5ed7cca6e0b1f93ebfcce1
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-08 14:15:00 -0700

    C pointers arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,11 +7,14 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0;
-	int j = n - 1;
+	int j = n -1;
+	int tmp;
 
-	while (i < n)
+	while (i < n / 2)
 	{
+		tmp = a[i];
 		a[i] = a[j];
+		a[j] = tmp;
 		i++;
 		j--;
 	}
