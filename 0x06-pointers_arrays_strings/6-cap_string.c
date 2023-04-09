82e54d8fc48e4742015cf3ad44ef9b4ef67fb72e
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-09 01:47:52 -0700

    C pointers arrays and strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *cap_string - function that capitalizes all words of a string
+ * letters of a string to uppercase.
+ * @n: input sting
+ * Return: capitalized string
+ */
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] == ' ' || n[i] == '\n' || n[i] == ',' || n[i] == ';')
+			n[i + 1] = n[i + 1] - 32;
+		if (n[i] == '.' || n[i] == '!' || n[i] == '?' || n[i] == '"')
+			n[i + 1] = n[i + 1] - 32;
+		if (n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
+			n[i + 1] = n[i + 1] - 32;
+		i++;
+	}
+	return (n);
+}
