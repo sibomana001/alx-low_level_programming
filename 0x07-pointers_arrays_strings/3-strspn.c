2503eba4a51a37b45d03bfa30f10deef5893dd2b
sibomana001 <sibomana.adolphe007@gmail.com>
2023-04-10 05:23:59 -0700

    C pointers arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * int _strspn - function that gets the length of a prefix substring
+ * @s: string_one
+ * @accept: string_two
+ * Return: bytes in segment of s which consist only of bytes frm accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, n = 1;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				n++;
+			}
+		}
+		return (n);
+	}
+	return (0);
+}
